e89a3e821be9b767da86fe45e69abe0b1a1db39d
Yusupova Muxlisa <umuhlisa716@gmail.com>
2024-11-11 17:10:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,208 @@
+// 1
+
+// let a = 5;
+// if (a > 0)
+//     console.log(++a);
+
+// else
+
+//     console.log(a);
+
+// 2
+
+// let a = 5;
+//  if (a > 0)
+//     console.log(++a);
+// else
+//     console.log(a - 2);
+
+// 3
+
+// let a = 0;
+
+// if (a > 0) {
+//     console.log(++a);
+
+// } else if (a < 0) {
+//     console.log(a - 2);
+
+// } else {
+//     console.log(10);
+// }
+
+// 6
+
+// let a = 75;
+
+// let b = 76;
+
+// console.log(Math.max(a,b));
+
+// 8
+
+// let a = 89
+
+// let b = 56
+
+// console.log(Math.max(a,b))
+// console.log(Math.min(a,b))
+
+// 9  ❌
+
+// let a = (Math.max(32));
+
+// let b = (Math.min(445));
+
+// console.log(Math.min(a, b));
+
+// 10
+
+// let a = 3;
+
+// let b = 3;
+
+// if (a !=b ) {
+//     console.log(a + b);
+// } else if ( a == b) {
+//     console.log(0);
+// }
+
+// 11
+
+// let a = 66;
+
+// let b = 78;
+
+// if (a != b) {
+//     console.log(Math.max(a,b));
+// }
+
+// else {
+//     console.log(0);
+// }
+
+// 12
+
+// let a = 34;
+
+// let b = 74;
+
+// let c = 98;
+
+// console.log(Math.min(a, b, c));
+
+// 13  ❌
+
+// let a = 74;
+
+// let b = 98;
+
+// let c = 34;
+
+// if (a > c < b) {
+//   console.log(b);
+// } else if (c > a < b) {
+//   console.log(b);
+// } else if (a > b < c) {
+//     console.log(c);
+// } else if (b > a < c) {
+//   console.log(c);
+// } else if (b > c < a) {
+//     console.log(a);
+// } else if (c > b < a) {
+//   console.log(a);
+// }
+
+
+// 14
+
+// let a = 52;
+
+// let b = 23;
+
+// console.log(Math.min(a, b));
+
+// console.log(Math.max(a, b))
+
+// 16 
+
+// let a = 83;
+
+// let b = 45;
+
+// let c = 62;
+
+// if (a < b && a < c && b > a && b < c) {
+//   console.log(a * 2, b * 2, c * 2);
+// }
+
+// else {
+//   console.log(0)
+// }
+
+// 17
+
+// let a = 83;
+
+// let b = 75;
+
+// let c = 62;
+
+// if (a < b && a < c && b > a && b < c) {
+//   console.log(a * 2, b * 2, c * 2);
+// }
+
+// else if (a > b && a > c && b > c) {
+//   console.log(a * 2, b * 2, c * 2);
+// }
+
+// else {
+//   console.log(0);
+// }
+
+// let a = 83;
+
+// let b = 83;
+
+// let c = [62];
+
+// console.log(c[])
+
+
+
+// 1
+
+// let a = 4;
+
+// console.log(a * 4);
+
+// 2
+
+// let a = 5;
+
+// console.log(a * a);
+
+
+// 3
+
+// let a = 24;
+
+// let b = 11;
+
+// let s = a * b;
+
+// let p = 2 * (a + b);
+
+// console.log("To'rburchak yuzasi", s);
+
+// console.log("To'rburchak perimetri", p)
+
+// 4
+
+let d = 10;
+
+let p = 3.14;
+
+let l = p * d;
+
+console.log(p * d);
